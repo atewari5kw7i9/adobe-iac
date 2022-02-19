08c842bdbb7c7ac0e2faf776f02135ebff4b7da3
xaxt176 <aditya.tewari@grainger.com>
2022-02-18 23:53:41 -0600

    Lambda

--- lambda.tf
+++ lambda.tf
@@ -18,13 +18,7 @@ resource "aws_lambda_function" "adobe_data_processor" {
   timeout       = 300
   environment {
     variables = {
-      emr_cluster_id   = "abc",
-      output_path      = "s3://logs-adobe-outbound/data",
-      executor-memory  = "1G",
-      driver-memory    = "1G",
-      job-name         = "job_transform",
-      code-artifacts   = "s3://adobe-code-artifacts",
-      jar-file         = "s3://us-east-1.elasticmapreduce/libs/script-runner/script-runner.jar"
+      emr_cluster_id   = "abc"
     }
   }
   handler       = "main.lambda_handler"
